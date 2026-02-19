44adfd9d5cc44a8d14261d31372ef6d384521963
Yaswanth Reddy Bobbiti(UST <307366@ust.com>
2026-02-19 10:34:51 +0530

    Change-1

--- Employee/main.py
+++ Employee/main.py
@@ -1,44 +1,43 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-from typing import Optional
+
 app = FastAPI()
-employees = []
+employees = [] 
 
 class Employee(BaseModel):
-	id: int
-	name: str
-	role: str
+    id: int
+    name: str
+    role: str
 
 @app.get('/')
 def get_all_employees():
-	return employeess
+    return employees
 
 @app.post('/add')
-def add_employee(e:Employee):
-	employees.append({'id':e.id, 'name':e.name, 'role':e.role})
-	return {'message':'Employee added successfully'}
+def add_employee(e: Employee):
+    employees.append({'id': e.id, 'name': e.name, 'role': e.role})
+    return {'message': 'Employee added successfully'}
 
 @app.get('/get_details')
-def get_employee(id:int):
-	for e in employees:
-		if e['id'] == id:
-			return e
-	
-	return {'message':'Employee not found'}
-
-@app.delete('/delete')
-def delete_employee(id:int):
-    for e in employees:
-		if e['id'] == id:
-			del employees[employees.index(e)]
-            return {'message':'Employee deleted successfully'}
+def get_employee(id: int):
+    for emp in employees:
+        if emp['id'] == id:
+            return emp
+    return {'message': 'Employee not found'}
 
 @app.put('/update')
-def update_employee(id:int, name:str, role:str):
-    for e in employees:
-        if e['id'] == id:
-            e['name'] = name
-            e['role'] = role
-            return {'message':'Employee updated successfully'}
-    return {'message':'Employee not found'}
+def update_employee(id: int, name: str, role: str):
+    for emp in employees:
+        if emp['id'] == id:
+            emp['name'] = name
+            emp['role'] = role
+            return {'message': 'Employee updated successfully'}
+    return {'message': 'Employee not found'}
 
+@app.delete('/delete')
+def delete_employee(id: int):
+    for emp in employees:
+        if emp['id'] == id:
+            employees.remove(emp)
+            return {'message': 'Employee deleted successfully'}
+    return {'message': 'Employee not found'}
